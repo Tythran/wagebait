4856b0680366859fb050047b4f86e0341b711b9b
Sam Farhady <129229954+samfarhady@users.noreply.github.com>
2025-03-30 09:35:13 -0400

    changing colors

--- wagebait/src/app/avatar/page.jsx
+++ wagebait/src/app/avatar/page.jsx
@@ -1,11 +1,11 @@
 import { useMemo } from 'react';
 import { createAvatar } from '@dicebear/core';
-import { bottts } from '@dicebear/collection';
+import { openPeeps } from '@dicebear/collection';
 
 function App() {
   const seed = 'player2';
   const avatar = useMemo(() => {
-    return createAvatar(bottts, {
+    return createAvatar(openPeeps, {
       seed: seed,
       size: 200
     }).toDataUri();
