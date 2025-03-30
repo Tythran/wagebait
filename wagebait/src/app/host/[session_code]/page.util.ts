eda70d80af0d12005c71525b0def06696b173d98
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-03-30 13:42:36 -0400

    feat: implement player subscription channels and update betting pool logic

--- wagebait/src/app/host/[session_code]/page.util.ts
+++ wagebait/src/app/host/[session_code]/page.util.ts
@@ -1,20 +1,20 @@
-import { createClient } from '@/utils/supabase/server';
+// import { createClient } from '@/utils/supabase/server';
 
-const nextPlayer = async (playerID: string, sessionCode: string): Promise<string | null> => {
-  const supabase = await createClient();
+// const nextPlayer = async (playerID: string, sessionCode: string): Promise<string | null> => {
+//   const supabase = await createClient();
 
-  const { data: playerNumbers, error: fetchPlayersError } = await supabase
-    .from('active_players')
-    .select('player_number, player_id')
-    .eq('session_code', sessionCode)
-    .overrideTypes<Array<{ player_id: string; player_number: number }>>();
-  if (fetchPlayersError) {
-    console.error(fetchPlayersError);
-  } else {
-    playerNumbers.sort((a, b) => a.player_number - b.player_number);
-    const curPlayerIdx = playerNumbers.findIndex((player) => player.player_id === playerID);
-    const nextPlayerIdx = (curPlayerIdx + 1) % playerNumbers.length;
-    return playerNumbers[nextPlayerIdx].player_id;
-  }
-  return null;
-};
+//   const { data: playerNumbers, error: fetchPlayersError } = await supabase
+//     .from('active_players')
+//     .select('player_number, player_id')
+//     .eq('session_code', sessionCode)
+//     .overrideTypes<Array<{ player_id: string; player_number: number }>>();
+//   if (fetchPlayersError) {
+//     console.error(fetchPlayersError);
+//   } else {
+//     playerNumbers.sort((a, b) => a.player_number - b.player_number);
+//     const curPlayerIdx = playerNumbers.findIndex((player) => player.player_id === playerID);
+//     const nextPlayerIdx = (curPlayerIdx + 1) % playerNumbers.length;
+//     return playerNumbers[nextPlayerIdx].player_id;
+//   }
+//   return null;
+// };
