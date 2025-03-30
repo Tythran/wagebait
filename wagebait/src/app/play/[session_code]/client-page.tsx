27bb12a15cdeac3bbd0ef01e367aa2f039ead551
atlantisel <80100913+atlantisel@users.noreply.github.com>
2025-03-30 00:08:08 -0400

    refactor: comment out unused state variables in client page

--- wagebait/src/app/play/[session_code]/client-page.tsx
+++ wagebait/src/app/play/[session_code]/client-page.tsx
@@ -4,8 +4,8 @@ import Buttons from './buttons';
 
 export default function Client({ sessionCode }: { sessionCode: string }) {
   console.log(sessionCode);
-  const [playerName, setPlayerName] = useState<string>('');
-  const [avatarSeed, setAvatarSeed] = useState<string>('');
+  // const [playerName, setPlayerName] = useState<string>('');
+  // const [avatarSeed, setAvatarSeed] = useState<string>('');
 
   return <Buttons />;
 }
